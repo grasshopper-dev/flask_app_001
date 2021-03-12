44f6e73031a67f9e7776166abbba4220734eee0b
grasshopper-dev <grasshopperdevel2019@gmail.com>
2021-03-12 10:50:38 +0900

    Jinjaテンプレートの利用検証

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', title="Index with Jinja", message="これはJinjaテンプレートの利用例です。！")
 
 if __name__ == '__main__':
     app.debug = True
