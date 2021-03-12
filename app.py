48abdb3b45559a1eb66bf45f5e8a25cb022397d0
grasshopper-dev <grasshopperdevel2019@gmail.com>
2021-03-12 11:24:11 +0900

    form送信の検証

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', title="Index with Jinja", message="これはJinjaテンプレートの利用例です。！")
+    return render_template('index.html', title="Form sample", message="お名前は？")
 
-@app.route('/<id>/<password>')
-def index2(id, password):
-    msg = 'id: %s, password: %s' %(id, password)
-    return render_template('index.html', title="Index with Jinja", message=msg)
+@app.route('/', methods=['POST'])
+def form():
+    field = request.form['field']
+    return render_template('index.html', title="Form sample", message="こんにちは、%sさん！" % field)
 
 
 if __name__ == '__main__':
