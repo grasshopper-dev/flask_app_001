019461c280fd8ae1eaa0b6b9ee218a2362b48919
grasshopper-dev <grasshopperdevel2019@gmail.com>
2021-03-12 11:11:01 +0900

    パラメータ渡し検証

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title="Index with Jinja", message="これはJinjaテンプレートの利用例です。！")
 
+@app.route('/<id>/<password>')
+def index2(id, password):
+    msg = 'id: %s, password: %s' %(id, password)
+    return render_template('index.html', title="Index with Jinja", message=msg)
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='localhost')
