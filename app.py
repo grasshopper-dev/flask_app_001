a1ed1a9946aad479a569c613763643bcb75d1c50
grasshopper-dev <grasshopperdevel2019@gmail.com>
2021-03-15 11:14:04 +0900

    コンテキストプロセッサ

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    n = random.randrange(5, 10)
-    data = []
-    for n in range(n):
-        data.append(random.randrange(0, 100))
-    return render_template('index.html', title="Form sample", message='※合計を計算する：', data=data)
+    # n = random.randrange(5, 10)
+    # data = []
+    # for n in range(n):
+    #     data.append(random.randrange(0, 100))
+    return render_template('index.html', title="Form sample", message='※合計を計算する：')
 
 @app.route('/', methods=['POST'])
 def form():
@@ -30,6 +30,15 @@ def sum_filter(data):
     return total
 
 app.jinja_env.filters['sum'] = sum_filter
+
+@app.context_processor
+def sample_processotr():
+    def total(n):
+        total = 0
+        for i in range(n + 1):
+            total += i
+        return total
+    return dict(total=total)
 
 if __name__ == '__main__':
     app.debug = True
